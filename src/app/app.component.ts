b7b03b277cc5224a044612fdd672c440dcd5fe72
Haroon Rasheed Butt <haroon.rasheed@d-p-s.com>
2022-01-07 21:57:45 +0500

    completed the tutorial. Done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import { Status } from './enum/status.enum';
 import { AppState } from './interface/app-state';
 import { CustomResponse } from './interface/custom-response';
 import { Server } from './interface/server';
+import { NotificationService } from './service/notification.service';
 import { ServerService } from './service/server.service';
 
 @Component({
@@ -22,21 +23,24 @@ export class AppComponent implements OnInit {
   dataSubject = new BehaviorSubject<CustomResponse>(null);
   private filterSubject = new BehaviorSubject<string>('');
   filterStatus$ = this.filterSubject.asObservable();
-
   private isLoading = new BehaviorSubject<boolean>(false);
-  isLoading$ = this.filterSubject.asObservable();
+  isLoading$ = this.isLoading.asObservable();
 
-  constructor(private serverService: ServerService) { }
+  constructor(private serverService: ServerService, private notifier: NotificationService) { }
 
   ngOnInit(): void {
     this.appState$ = this.serverService.servers$
       .pipe(
         map(response => {
+          this.notifier.onDefault(response.message);
           this.dataSubject.next(response);
-          return { dataState: DataState.LOADED_STATE, appData: response }
+          return {
+            dataState: DataState.LOADED_STATE, appData: { ...response, data: { servers: response.data.servers.reverse() } }
+          }
         }),
         startWith({ dataState: DataState.LOADING_STATE }),
         catchError((error: string) => {
+          this.notifier.onError(error);
           return of({ dataState: DataState.ERROR_STATE, error: error })
         })
       );
@@ -47,6 +51,7 @@ export class AppComponent implements OnInit {
     this.appState$ = this.serverService.ping$(ipAddress)
       .pipe(
         map(response => {
+          this.notifier.onDefault(response.message);
           const index = this.dataSubject.value.data.servers?.findIndex(server => server.id === response.data.server.id);
           // const index = this.dataSubject.value.data.servers?.findIndex(server => server.ipAddress === ipAddress);
           this.dataSubject.value.data.servers[index] = response.data.server;
@@ -55,6 +60,7 @@ export class AppComponent implements OnInit {
         }),
         startWith({ dataState: DataState.LOADED_STATE, appData: this.dataSubject.value }),
         catchError((error: string) => {
+          this.notifier.onError(error);
           this.filterSubject.next('');
           return of({ dataState: DataState.ERROR_STATE, error: error })
         })
@@ -66,26 +72,27 @@ export class AppComponent implements OnInit {
     this.appState$ = this.serverService.filter$(status, this.dataSubject.value)
       .pipe(
         map(response => {
+          this.notifier.onDefault(response.message);
           return { dataState: DataState.LOADED_STATE, appData: response }
         }),
         startWith({ dataState: DataState.LOADED_STATE, appData: this.dataSubject.value }),
         catchError((error: string) => {
+          this.notifier.onError(error);
           return of({ dataState: DataState.ERROR_STATE, error: error })
         })
       );
   }
 
   saveServer(serverForm: NgForm): void {
+    this.isLoading.next(true);
     this.appState$ = this.serverService.save$(serverForm.value as Server)
       .pipe(
         map(response => {
-          this.isLoading.next(true);
-          // What im gonna do is push on new object in the data subject. Or we can say that we can just
-          // change the information inside the dataObject. 
           this.dataSubject.next(
             { ...response, data: { servers: [response.data.server, ...this.dataSubject.value.data.servers] } }
           );
-          document.getElementById('closeModal').click;
+          this.notifier.onDefault(response.message);
+          document.getElementById('closeModal').click();
           this.isLoading.next(false);
           serverForm.resetForm({ status: this.Status.SERVER_DOWN });
           return { dataState: DataState.LOADED_STATE, appData: this.dataSubject.value }
@@ -93,8 +100,48 @@ export class AppComponent implements OnInit {
         startWith({ dataState: DataState.LOADED_STATE, appData: this.dataSubject.value }),
         catchError((error: string) => {
           this.isLoading.next(false);
+          this.notifier.onError(error);
+          return of({ dataState: DataState.ERROR_STATE, error });
+        })
+      );
+  }
+
+  deleteServer(server: Server): void {
+    this.appState$ = this.serverService.delete$(server.id)
+      .pipe(
+        map(response => {
+          this.dataSubject.next(
+            { ...response, data: { servers: this.dataSubject.value.data.servers.filter(s => s.id != server.id) } }
+          );
+          this.notifier.onDefault(response.message);
+          return { dataState: DataState.LOADED_STATE, appData: this.dataSubject.value }
+        }),
+        startWith({ dataState: DataState.LOADED_STATE, appData: this.dataSubject.value }),
+        catchError((error: string) => {
+          this.notifier.onError(error);
           return of({ dataState: DataState.ERROR_STATE, error: error })
         })
       );
+  }
+
+  printReport(): void {
+
+    let dataType = 'application/vnd.ms-excel.sheet.macroEnabled.12';
+    // we need to define table so we doin that.. We can grab the table from html as an element here  
+    let tableSelect = document.getElementById("servers");
+    // we want this in encoded url so ... we want in url encoded format... replacing cuz we need to replace the space with the encoded url value of the space. 
+    let tableHtml = tableSelect.outerHTML.replace(/ /g, '%20');
+    // we need a link to download. but we not gonna show this to uI
+    let downloadLink = document.createElement('a');
+    document.body.appendChild(downloadLink);
+    downloadLink.href = 'data:' + dataType + ', ' + tableHtml;
+    downloadLink.download = 'server-report.xls';
+    downloadLink.click();
+    document.body.removeChild(downloadLink);
+
+    /*comment all the prevoius code in this function to print the report in pdf thought this function*/
+    // window.print();
+
+    this.notifier.onDefault("Report Downloaded");
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms'
+import { FormsModule } from '@angular/forms';
+import { NotificationModule } from './notification.module';
 
 @NgModule({
   declarations: [
@@ -10,8 +12,10 @@ import { FormsModule } from '@angular/forms'
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    NotificationModule
   ],
   providers: [],
   bootstrap: [AppComponent]
